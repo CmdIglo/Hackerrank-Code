a289feedf7095c9866a5b8e003e04d5ef2209951
CmdIglo <77732781+CmdIglo@users.noreply.github.com>
2021-11-23 19:56:28 +0100

    Update Compress_the_String.py

--- Compress_the_String.py
+++ Compress_the_String.py
@@ -19,11 +19,19 @@ All the characters of S denote integers between 0 and 9.
 
 #  - - - - - - - - - - - - - - - - - - Code - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
 
+# The challenge was about the itertools.groupby() function. It takes an iterable as an argument and returns the consecutive occurences 
+# of a value in the iterable along with the value itself i.e.: [1, 1, 2, 3, 3, 3, 4, 4] => (1, 2) (2, 1) (3, 3) (4, 2)
 from itertools import groupby
 
 def main():
+    
+    # input as a string, for example "111133426666"
     n = input()
+    # formatting the string into an iterable i.e. list
     nLis = [i for i in n]
+    
+    # for every value in nLis it's occurences and the value itself gets returned and printed in the format : (occurences, value)
+    # Note that the value mentioned above is "key" in this example and the number of occurences is value (len(value))
     for key, value in groupby(nLis):
         print("({}, {})".format(len(list(value)), key), end=" ")
         
